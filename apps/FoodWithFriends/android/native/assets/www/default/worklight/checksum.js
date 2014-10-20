15d0e3c27c2880899ece3be4130ae821147a1cdc
macked3 <macked3@nationwide.com>
2014-10-20 19:33:39 -0400

    Refactored the file structure a bit.

--- apps/FoodWithFriends/android/native/assets/www/default/worklight/checksum.js
+++ apps/FoodWithFriends/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":4209805211,"date":1413659490009,"machine":"NW102426"};
-/* Date: Sat Oct 18 15:11:30 EDT 2014 */
+var WL_CHECKSUM = {"checksum":1595635146,"date":1413846212387,"machine":"NW102426"};
+/* Date: Mon Oct 20 19:03:32 EDT 2014 */
